4c473db724a654ac37937ccbe61baeac57f6d16f
Yoshihisa AMAKATA <amakata@flatz.jp>
2013-10-30 23:28:47 +0900

    screenの設定とemacsの設定を追加した

--- site-cookbooks/base/recipes/default.rb
+++ site-cookbooks/base/recipes/default.rb
@@ -2,8 +2,46 @@
 # Cookbook Name:: base
 # Recipe:: default
 #
-# Copyright 2013, cmmpower
+# Copyright 2013, Yoshihisa AMAKATA<amakata@gmail.com>
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+# 開発ツール関連
+%w{screen vim emacs}.each do |pkg|
+ package pkg do
+  action :install
+ end
+end
+
+directory "/home/pi/.emacs.d" do
+ owner 'pi'
+ group 'pi'
+ mode '0775'
+ action :create
+end
+
+%w{home/pi/.screenrc home/pi/.emacs.d/init.el}.each do |f|
+ cookbook_file "/"+f do
+  mode "0644"
+  owner "pi"
+  group "pi"
+  source f
+ end
+end
+
+directory "/root/.emacs.d" do
+ owner 'root'
+ group 'root'
+ mode '0775'
+ action :create
+end
+
+# opencv 関連
+%w{python-opencv python-scipy python-numpy python-pip python-pygame}.each do |pkg|
+ package pkg do
+  action :install
+ end
+end
 
